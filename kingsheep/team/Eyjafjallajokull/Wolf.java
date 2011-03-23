f318d15730afff8875520095aab858f5007cffc4
Andreas Wessel Larsen <larsenaw@hotmail.com>
2011-03-23 18:45:24 +0100

    fiksa ClosestGrassFarAway ikke henger seg nar alt gresset er borte

--- kingsheep/team/Eyjafjallajokull/Wolf.java
+++ kingsheep/team/Eyjafjallajokull/Wolf.java
@@ -13,12 +13,12 @@ public class Wolf extends AI {
 		protectGrass = 0;
 
 		a = new Algorithm[1];
-		//a[0] = new Random();
+		a[0] = new Random();
 		//a[0] = new BestClose();
 		//a[2] = new keepSameDirection();
 		//a[3] = new DontStandStill();
 		//a[4] = new DontMoveBack();
-		a[0] = new TowardBest();
+		//a[0] = new TowardBest();
     }
 
     public int rateField(int ry, int rx) {
